be97b6403d4fb74ecd5d84df5fa33c3bef507682
Matt <info@mgates.me>
2012-09-16 19:05:06 -0400

    More comments, init.js

--- source/classes/init.js
+++ source/classes/init.js
@@ -8,31 +8,33 @@ $(document).ready(function () {
         // Remove advertisements
         remove_adverts();
 
-        // Equipment Fix
+        // Equipment fix
         if (document.URL.indexOf("equipment.php") > 0) {
             equipment_fixes_init();
         }
 
-        // Investment Fix
+        // Investment fix
         if (document.URL.indexOf("investment.php") > 0) {
             investment_fixes_init();
         }
 
-        // Loot Fix
+        // Loot fix
         if (document.URL.indexOf("selectedTab=loot") > 0) {
             loot_fixes_init();
         }
 
-        // Bank Fix
+        // Bank fix
         if (document.URL.indexOf("bank.php") > 0) {
             bank_fixes_init();
         }
 
+        // Deposit all your monies
         $('#bank_auto').click(function(e) {
             e.preventDefault();
             bank_auto();
         });
 
+        // Auto invest in real estate
         $('#investment_auto').click(function(e) {
             e.preventDefault();
             investment_auto();
