131bbeef496e2d46c3ef9382d1d88d47e54e6939
Matt <info@mgates.me>
2012-09-16 14:33:20 -0400

    Loot fixes function separation

--- source/classes/loot/class-loot-fixes.js
+++ source/classes/loot/class-loot-fixes.js
@@ -1,9 +1,16 @@
 /**
+ * Begin fixing the loot page!
+ */
+function loot_fixes_init() {
+
+	loot_fix_sell_links();
+
+}
+
+/**
  * Fix sell links to work on the Loot page.
  */
-function loot_fixes() {
-
-    // javascript:sellEquipmentDialog('profile.php?x=1&selectedTab=loot', 114, 0, 'Enemy Plans', 1, 'd0013ead3db3cfb51b19aa97ebd1608abe5d31df', 'sell')
+function loot_fix_sell_links() {
 
     $('a[href^="javascript:sellEquipmentDialog"]').each(function (index) {
         var values = $.map(this.href.split(","), $.trim);
