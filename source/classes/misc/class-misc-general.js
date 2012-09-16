6d1e787f69c15239f08c760363268f59058d4e11
Matt <info@mgates.me>
2012-09-16 19:03:07 -0400

    Comments on class-misc-general.js

--- source/classes/misc/class-misc-general.js
+++ source/classes/misc/class-misc-general.js
@@ -1,11 +1,28 @@
+/**
+ * Total on hand cash of the user.
+ *
+ * On hand means cash not in the bank.
+ * @return {float} Numerical value of on hand cash.
+ */
 function get_current_cash() {
     return format_number($('#cashCurrent').text());
 }
 
+/**
+ * Level of the user.
+ * @return {string} Level represented as a string.
+ */
 function get_current_level() {
     return $('.levelFrontTopArea').text();
 }
 
+/**
+ * Formats any ol' numba from a string into a float.
+ *
+ * Removes commas, dollar signs, and any other non 0-9 char.
+ * @param  {string} numba The number to convert.
+ * @return {float}       Number as a float.
+ */
 function format_number(numba) {
 
     numba = numba.replace(/[$,]/g,'');
